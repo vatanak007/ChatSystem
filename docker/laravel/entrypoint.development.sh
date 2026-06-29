379a1c30319aab20beaab5a4fac3fcd2f8c44490
vatanak007 <vatanak080@gmail.com>
2026-06-30 03:15:59 +0700

    edit entrypoint laravel & vuejs, AuthController and add restart to compose.yaml

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-composer install
-wait $!
+# composer install
+# wait $!
+
 php artisan key:generate
 wait $!
+
 php artisan migrate
 wait $!
+
 php artisan serve --host=0.0.0.0 --port=8000
 
 
--- laravel-app/app/Http/Controllers/API/AuthController.php
+++ laravel-app/app/Http/Controllers/API/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => Hash::make($request->password),
         ]);
 
         return response([
