9cf0cd6689868c44b08339eaaef834dee75a5470
vatanak007 <vatanak080@gmail.com>
2026-07-30 07:56:41 +0700

    Password reset backend

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -10,6 +10,8 @@ Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware('signed')
     ->name('verify.email');
 Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
+Route::post('/send/reset-password-email', [AuthController::class, 'sendResetPasswordEmail']);
+Route::post('/set/new-password', [AuthController::class, 'setNewPassword'])->name('set.new-password');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
