037cb8cad260e39e34515a96eb1fc9c081afd4ce
vatanak007 <vatanak080@gmail.com>
2026-07-31 10:14:02 +0700

    Google OAuth sign-in via Laravel Socialite

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -1,17 +1,23 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\GoogleOAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/signin', [AuthController::class, 'signin']);
-
 Route::get('/verify/email/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware('signed')
     ->name('verify.email');
 Route::post('/send/verification-email', [AuthController::class, 'sendVerificationEmail']);
 Route::post('/send/reset-password-email', [AuthController::class, 'sendResetPasswordEmail']);
 Route::post('/set/new-password', [AuthController::class, 'setNewPassword'])->name('set.new-password');
+
+Route::prefix('google')->group(function () {
+    Route::get('/oauth/redirect', [GoogleOAuthController::class, 'googleOAuthRedirect']);
+    Route::get('/oauth/callback', [GoogleOAuthController::class, 'googleOAuthCallback']);
+    Route::post('/oauth/exchange/token', [GoogleOAuthController::class, 'googleOAuthExchangeToken'])->middleware('auth:sanctum');
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/signout', [AuthController::class, 'signout']);
